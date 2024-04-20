fe893ba89e161c423f28337c269a4885b0dce713
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-04-20 19:44:38 +0530

    modify

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,16 @@ import { Header } from './components/header/Header';
 import { Home } from './components/home/Home';
 import { Box } from '@mui/material';
 
-
+import { DataProvider } from './context/DataProvider';
 
 function App() {
   return (
-    <div>
+    <DataProvider>
        <Header/>
-
        <Box style={{marginTop: 54}}>
          <Home/>
        </Box>
-    </div>
+    </DataProvider>
   );
 }
 
