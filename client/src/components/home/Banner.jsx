2a88d8ba793b5df8c11a8824226f991d10355476
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-04-27 13:28:46 +0530

    responsive

--- client/src/components/home/Banner.jsx
+++ client/src/components/home/Banner.jsx
@@ -4,10 +4,14 @@ import { bannerData } from "../../constants/data";
 import { styled } from "@mui/material";
 
 
-const Image = styled('img')({
+const Image = styled('img')(({theme}) => ({
     width: '100%',
-    height: 280
-});
+    height: 280,
+    [theme.breakpoints.down('md')]: {
+      objectFit: 'cover',
+      height: 180
+    }
+}));
 
 const responsive = {
    
