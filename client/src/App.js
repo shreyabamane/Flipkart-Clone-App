8129115f6f1dea9b307ef43215e31c69442a2ee7
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-04-27 14:17:40 +0530

    wrap all components in BrowserRouter Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
 import { Header } from './components/header/Header';
 import { Home } from './components/home/Home';
 import { Box } from '@mui/material';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { DataProvider } from './context/DataProvider';
+import { DetailView } from './components/details/DetailView';
 
 function App() {
   return (
     <DataProvider>
-       <Header/>
-       <Box style={{marginTop: 54}}>
-         <Home/>
-       </Box>
+      <BrowserRouter>
+        <Header />
+        <Box style={{ marginTop: 54 }}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/product/:id' element={<DetailView/>} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
     </DataProvider>
   );
 }
