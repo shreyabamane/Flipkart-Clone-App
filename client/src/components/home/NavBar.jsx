3096d8b12af20ea5353061824c7d13a2621333a0
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-04-27 13:31:10 +0530

    responsive

--- client/src/components/home/NavBar.jsx
+++ client/src/components/home/NavBar.jsx
@@ -1,11 +1,16 @@
 import { Box, Typography, styled } from "@mui/material";
 import { navData } from "../../constants/data";
 
-const Component = styled(Box)`
-   display: flex;
-   margin: 55px 130px 0 130px;
-   justify-content: space-between;
-`;
+const Component = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    margin: '55px 130px 0 130px',
+    justifyContent: 'space-between',
+    // overflow: 'overlay',
+    overflow: 'hidden',
+    [theme.breakpoints.down('lg')]: {
+        margin: 0
+    }
+}));
 
 const Container = styled(Box)`
    padding: 12px 8px;
@@ -19,13 +24,13 @@ const Text = styled(Typography)`
 `;
 
 
-export function NavBar(){
+export function NavBar() {
     return (
         <Component>
             {
-                navData.map(data =>(
+                navData.map(data => (
                     <Container>
-                        <img src={data.url} alt="nav" style={{width: 64}} />
+                        <img src={data.url} alt="nav" style={{ width: 64 }} />
                         <Text>{data.text}</Text>
                     </Container>
                 ))
