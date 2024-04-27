e28e5144fe67419914734a6c9b6f1e746eae4ac0
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-04-27 13:29:38 +0530

    responsive

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -6,6 +6,7 @@ import {useDispatch, useSelector} from "react-redux";
 import {getProducts} from "../../redux/actions/productActions";
 import { Slide } from "./Slide";
 import { MidSlide } from "./MidSlide";
+import { MidSection } from "./MidSection";
 
 const Component = styled(Box)`
    padding: 10px 10px;
@@ -35,6 +36,7 @@ export function Home(){
       <Component>
          <Banner/>
          <MidSlide products={products} title= 'Deal of the Day' timer={true} />
+         <MidSection />
          <Slide products={products} title= 'Discounts for You' timer={false} />
          <Slide products={products} title= 'Suggesting Items' timer={false} />
          <Slide products={products} title= 'Top Selection' timer={false} />
