5816b07175d2e78321b7ca69950d36491ee4e602
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-05-02 18:01:26 +0530

    add list and drawer in header for responsiveness

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,10 @@
-import { AppBar, Box, Toolbar, Typography, styled } from "@mui/material";
+import { AppBar, Box, Drawer, IconButton, List, ListItem, Toolbar, Typography, styled } from "@mui/material";
+import { Menu } from '@mui/icons-material';
+
 import { Search } from "./Search";
 import { CustomButtons } from "./CustomButtons";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const StyledHeader = styled(AppBar)`
     backgroung: #2874f0;
@@ -26,30 +29,69 @@ const PlusImage = styled('img')({
     marginLeft: 4,
 });
 
-const CustomButtonWrapper = styled(Box)`
-    margin: 0 5% 0 auto;
-`;
+const CustomButtonWrapper = styled(Box)(({ theme }) => ({
+    margin: '0 5% 0 auto',
+    [theme.breakpoints.down('md')]: {
+        display: 'none'
+    }
+}));
 
-export function Header(){
+const MenuButton = styled(IconButton)(({theme}) => ({
+    display: 'none',
+    [theme.breakpoints.down('md')]: {
+        display: 'block'
+    }
+}));
+
+
+export function Header() {
 
     const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
     const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
 
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    const list = () => (
+        <Box style={{width: 200}} onClick={handleClose}>
+            <List>
+                <ListItem button>
+                    <CustomButtons />
+                </ListItem>
+            </List>
+        </Box>
+    )
+
     return (
         <StyledHeader>
-            <Toolbar style={{minHeight: 55 }}>
+            <Toolbar style={{ minHeight: 55 }}>
+                <MenuButton color="inherit" onClick={handleOpen}>
+                    <Menu />
+                </MenuButton>
+
+                <Drawer open={open} onClose={handleClose}>
+                    {list()}
+                </Drawer>
+
                 <Component to='/'>
-                    <img src={logoURL} alt="logo" style={{width:75}} />
-                    <Box style={{display: 'flex'}}>
+                    <img src={logoURL} alt="logo" style={{ width: 75 }} />
+                    <Box style={{ display: 'flex' }}>
                         <SubHeading>Explore&nbsp;
-                            <Box component="span" style={{color:'#FFE500'}}>Plus</Box>
+                            <Box component="span" style={{ color: '#FFE500' }}>Plus</Box>
                         </SubHeading>
-                        <PlusImage src={subURL} alt="sub-logo" /> 
+                        <PlusImage src={subURL} alt="sub-logo" />
                     </Box>
                 </Component>
-                <Search/>
+                <Search />
                 <CustomButtonWrapper>
-                    <CustomButtons/>
+                    <CustomButtons />
                 </CustomButtonWrapper>
             </Toolbar>
         </StyledHeader>
