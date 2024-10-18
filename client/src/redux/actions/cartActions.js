d91bb68b844aee9daa2acf66726d512d46a6b78f
Shreya Prakash Bamane <72699072+shreyabamane@users.noreply.github.com>
2024-10-18 19:04:25 +0530

    update backend url

--- client/src/redux/actions/cartActions.js
+++ client/src/redux/actions/cartActions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import * as actionType from '../constants/cartConstant';
 
-const URL = 'http://localhost:8000';
+const URL = 'https://flipkart-clone-server-k65t.onrender.com';
 
 export const addToCart = (id, quantity) => async (dispatch) => {
     try {
