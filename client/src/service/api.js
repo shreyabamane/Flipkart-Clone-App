57b88ba29efbbfbb567e63485a2334d699b85672
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-10-18 19:07:43 +0530

    update backend url

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:8000'; 
+const URL = 'https://flipkart-clone-server-k65t.onrender.com'; 
 
 
 export async function authenticateSignup(data){
